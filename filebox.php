97fa211cc28679b47c2d04fdf7b56a88452db4c1
iikira <2571583272@qq.com>
2016-05-23 08:40:28 -0500

    Update filebox.php

--- filebox.php
+++ filebox.php
@@ -20,8 +20,8 @@ session_start();
 error_reporting(1);
 $filefolder = "./";
 $sitetitle = 'FileBox';
-$user = 'konica';
-$pass = 'Lollipop000';
+$user = 'admin';
+$pass = 'admin';
 $safe_num = 5;//设置多少次后禁止登陆，为0则不限制，建议为3-5
 $mail = 'i@hezi.be';//若有恶意登录，会发邮件到这个邮箱，前提是mail()函数可用！
 $meurl = $_SERVER['PHP_SELF'];
